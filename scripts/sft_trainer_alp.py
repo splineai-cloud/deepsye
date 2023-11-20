3bf9753fa54e68e15dd04831b10c69e1257e496c
splineai-cloud <53084798+splineai-cloud@users.noreply.github.com>
2023-11-21 02:48:57 +0530

    Update sft_trainer_alp.py

--- scripts/sft_trainer_alp.py
+++ scripts/sft_trainer_alp.py
@@ -45,7 +45,7 @@ class ScriptArguments:
         default=8, metadata={"help": "the number of gradient accumulation steps"}
     )
     load_in_8bit: Optional[bool] = field(default=False, metadata={"help": "load the model in 8 bits precision"})
-    load_in_4bit: Optional[bool] = field(default=True, metadata={"help": "load the model in 4 bits precision"})
+    load_in_4bit: Optional[bool] = field(default=False, metadata={"help": "load the model in 4 bits precision"})
     use_peft: Optional[bool] = field(default=True, metadata={"help": "Wether to use PEFT or not to train adapters"})
     trust_remote_code: Optional[bool] = field(default=True, metadata={"help": "Enable `trust_remote_code`"})
     output_dir: Optional[str] = field(default="output", metadata={"help": "the output directory"})
