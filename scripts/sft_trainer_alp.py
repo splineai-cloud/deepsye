a9e73ea8bccbe095a9de205fc26366e4bd7ee06f
splineai-cloud <53084798+splineai-cloud@users.noreply.github.com>
2023-11-21 01:35:04 +0530

    Update sft_trainer_alp.py

--- scripts/sft_trainer_alp.py
+++ scripts/sft_trainer_alp.py
@@ -91,7 +91,7 @@ dataset1 = Dataset.from_pandas(df)
 dataset=dataset1.train_test_split(test_size=0.1)
 
 def create_prompt_instruction(sample):
-   return sample["input_ids"] = f"""### Instruction:
+   sample["text"] = f"""### Instruction:
 {sample['instruction']}
 
 ### Input
@@ -99,6 +99,7 @@ def create_prompt_instruction(sample):
 
 ### Response:
    """
+return sample
 
 #dataset = load_dataset(script_args.dataset_name, split="train[:20000]")
 #dataset = dataset.train_test_split(test_size=0.1)
